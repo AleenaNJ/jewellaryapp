72653155c57a12935dbceaa8909de661060a221e
AleenaNJ <aleenanj12@gmail.com>
2024-02-09 09:00:24 +0530

    jewellary

---
+++ controller/jewellaryrouter.js
@@ -0,0 +1,19 @@
+const express=require("express")
+const jewellarymodel=require("../model/jewellarymodel")
+const router=express.Router()
+
+router.post("/add",async(req,res)=>{
+    let data=req.body
+    let jewellary=new jewellarymodel(data)
+    let result=await jewllary.save()
+    res.json({
+        status:"success"
+    })
+})
+
+
+
+
+
+
+module.exports=router
